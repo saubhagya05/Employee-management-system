02154270c7e938e07e219c404795993137af6d66
_recipr0cal_ <149934211+saubhagya05@users.noreply.github.com>
2024-12-25 03:37:17 +0530

    Add files via upload

---
+++ src/components/other/Header.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import { setLocalStorage } from '../../utils/localStorage'
+
+const Header = (props) => {
+
+  // const [username, setUsername] = useState('')
+
+  // if(!data){
+  //   setUsername('Admin')
+  // }else{
+  //   setUsername(data.firstName)
+  // }
+
+  const logOutUser = ()=>{
+    localStorage.setItem('loggedInUser','')
+    props.changeUser('')
+    // window.location.reload()
+  }
+
+  if(props.user=="admin"){
+  return (
+    <div className='flex items-end justify-between'>
+        <h1 className='text-2xl font-medium'>Hello <br /><span className='text-3xl font-semibold'>{props.user} 👋</span></h1>
+        <button onClick={logOutUser} className='bg-red-600 text-base font-medium text-white px-5 py-2 rounded-sm'>Log Out</button>
+    </div>
+  )
+}
+else{
+  return (
+    <div className='flex items-end justify-between'>
+        <h1 className='text-2xl font-medium'>Hello <br /><span className='text-3xl font-semibold'>{props.data.firstName} 👋</span></h1>
+        <button onClick={logOutUser} className='bg-red-600 text-base font-medium text-white px-5 py-2 rounded-sm'>Log Out</button>
+    </div>
+  )
+}
+}
+
+
+export default Header
